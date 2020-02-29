20ecc5dc714b8c296b677a2050821f7ad2495b7b
Igor Mozharovsky <igor.mozharovsky@gmail.com>
2020-03-01 00:40:30 +0200

    Warm shutdown improvements + tests

---
+++ tests/test_worker.py
@@ -0,0 +1,79 @@
+import asyncio
+import logging
+import signal
+from unittest.mock import MagicMock
+
+import pytest
+
+from darq import Darq
+from . import redis_settings
+
+darq_config = {'redis_settings': redis_settings}
+
+
+async def foobar():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_handle_sig_delayed(caplog, worker_factory):
+    caplog.set_level(logging.INFO)
+
+    darq = Darq(darq_config)
+    darq.task(foobar)
+
+    worker = await worker_factory(darq)
+
+    long_running_task_mock = MagicMock(done=MagicMock(return_value=False))
+    worker.main_task = MagicMock()
+    worker.tasks = [
+        MagicMock(done=MagicMock(return_value=True)),
+        long_running_task_mock,
+    ]
+
+    assert len(caplog.records) == 0
+    worker.handle_sig(signal.SIGINT)
+    await asyncio.sleep(0)
+    assert len(caplog.records) == 1
+    assert caplog.records[0].message == (
+        'Warm shutdown. Awaiting for 1 jobs with 30 seconds timeout.'
+    )
+    assert worker.main_task.cancel.call_count == 0
+    assert worker.tasks[0].cancel.call_count == 0
+    assert worker.tasks[1].cancel.call_count == 0
+    await asyncio.sleep(0)
+
+    worker.tasks[1].done.return_value = True
+    await asyncio.sleep(1)
+
+    assert 'shutdown on SIGINT' in caplog.records[1].message
+    assert worker.main_task.cancel.call_count == 1
+    assert worker.tasks[0].cancel.call_count == 0
+    assert worker.tasks[1].cancel.call_count == 0
+
+
+@pytest.mark.asyncio
+async def test_handle_sig_hard(event_loop, caplog, worker_factory):
+    caplog.set_level(logging.INFO)
+
+    darq = Darq(darq_config)
+    darq.task(foobar)
+
+    worker = await worker_factory(darq)
+
+    long_running_task_mock = MagicMock(done=MagicMock(return_value=False))
+    worker.main_task = MagicMock()
+    worker.tasks = [long_running_task_mock]
+
+    assert len(caplog.records) == 0
+    worker.handle_sig(signal.SIGINT)
+    await asyncio.sleep(0)
+    assert len(caplog.records) == 1
+    assert caplog.records[0].message == (
+        'Warm shutdown. Awaiting for 1 jobs with 30 seconds timeout.'
+    )
+    worker.handle_sig(signal.SIGINT)
+    assert caplog.records[1].message == (
+        'shutdown on SIGINT ◆ 0 jobs complete ◆ 0 failed ◆ 0 retries ◆ '
+        '1 ongoing to cancel'
+    )
