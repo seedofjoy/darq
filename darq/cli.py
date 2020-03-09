569cc3d5b1f17d6f6845ba7dd4fad82d8a3a2357
Igor Mozharovsky <igor.mozharovsky@gmail.com>
2020-03-09 20:10:52 +0200

    Add watch-mode to CLI. Add CLI-entrypoint

--- darq/cli.py
+++ darq/cli.py
@@ -1,27 +1,41 @@
+import asyncio
 import logging.config
 import os
 import sys
+import typing as t
+from signal import Signals
 
 import click
 from arq.logs import default_log_config
 from pydantic.utils import import_string
 
 from .app import Darq
+from .version import VERSION
 from .worker import check_health
+from .worker import create_worker
 from .worker import run_worker
 
 health_check_help = 'Health Check: run a health check and exit.'
+watch_help = 'Watch a directory and reload the worker upon changes.'
 verbose_help = 'Enable verbose output.'
 
 
 @click.command('darq')
+@click.version_option(VERSION, '-V', '--version', prog_name='darq')
 @click.argument('darq-app', type=str, required=True)
 @click.option('--check', is_flag=True, help=health_check_help)
+@click.option(
+    '--watch', type=click.Path(exists=True, dir_okay=True, file_okay=False),
+    help=watch_help,
+)
 @click.option('-v', '--verbose', is_flag=True, help=verbose_help)
 @click.option('-Q', '--queue', type=str, default=None)
-def cli(*, darq_app: str, check: bool, verbose: bool, queue: str) -> None:
+def cli(
+        *, darq_app: str, check: bool, watch: str, verbose: bool, queue: str,
+) -> None:
     """
     Job queues in python with asyncio and redis.
+
     CLI to run the darq worker.
 
     DARQ_APP - path to Darq app instance.
@@ -39,7 +53,48 @@ def cli(*, darq_app: str, check: bool, verbose: bool, queue: str) -> None:
     if check:
         exit(check_health(darq, queue))
     else:
-        run_worker(darq, queue)
+        if watch:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(watch_reload(watch, darq, queue, loop))
+        else:
+            run_worker(darq, queue)
+
+
+async def watch_reload(
+        path: str, darq: Darq, queue: str, loop: asyncio.AbstractEventLoop,
+) -> None:
+    try:
+        from watchgod import awatch
+    except ImportError as e:  # pragma: no cover
+        raise ImportError(
+            'watchgod not installed, use `pip install watchgod`',
+        ) from e
+
+    stop_event = asyncio.Event()
+    worker = create_worker(darq, queue)
+
+    tasks: t.List[asyncio.Task[t.Any]] = []
+
+    def cancel_tasks() -> None:
+        while tasks:
+            tasks.pop().cancel()
+
+    try:
+        worker.on_stop = (
+            lambda s: s != Signals.SIGUSR1 and stop_event.set()  # type: ignore
+        )
+        tasks.append(loop.create_task(worker.async_run()))
+        async for changes in awatch(path, stop_event=stop_event):
+            click.echo(
+                f'{len(changes)} files changed, reloading darq worker...',
+            )
+            worker.handle_sig(Signals.SIGUSR1)
+            await worker.close()
+            cancel_tasks()
+            tasks.append(loop.create_task(worker.async_run()))
+        cancel_tasks()
+    finally:
+        await worker.close()
 
 
 if __name__ == '__main__':
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+from aiohttp.test_utils import loop_context
 from click.testing import CliRunner
 
 from darq import Darq
@@ -25,3 +26,24 @@ def test_run():
     assert result.exit_code == 0
     cli_output = 'Starting worker for 1 functions: tests.test_cli.foobar'
     assert cli_output in result.output
+
+
+def test_check():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['tests.test_cli.darq', '--check'])
+    assert result.exit_code == 1
+    assert 'Health check failed: no health check sentinel value found' in result.output
+
+
+async def mock_awatch():
+    yield [1]
+
+
+def test_run_watch(mocker):
+    darq.redis_pool = None
+    mocker.patch('watchgod.awatch', return_value=mock_awatch())
+    runner = CliRunner()
+    with loop_context():
+        result = runner.invoke(cli, ['tests.test_cli.darq', '--watch', 'tests'])
+    assert result.exit_code == 0
+    assert '1 files changed, reloading darq worker...' in result.output
